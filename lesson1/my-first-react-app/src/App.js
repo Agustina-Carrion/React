864edda0cd412aba6a31a6a1b39eb7d42cbbb0b1
Agustina <agus.carrion@icloud.com>
2022-11-16 11:33:18 +0100

    added first component

--- lesson1/my-first-react-app/src/App.js
+++ lesson1/my-first-react-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MyFirstComponent from './MyFirstComponent';
 
 function App() {
   return (
     <div className="App">
+      <MyFirstComponent></MyFirstComponent>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ lesson1/my-first-react-app/src/MyFirstComponent.js
@@ -0,0 +1,10 @@
+function MyFirstComponent() {
+  return (
+    <>
+      <h1>This is my first component in React</h1>
+      <div>Testing different elements!</div>
+    </>
+  );
+}
+
+export default MyFirstComponent;
