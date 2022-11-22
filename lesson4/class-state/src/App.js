475b6a425a047cf706898c3179afbda41dc567bf
Agustina <agus.carrion@icloud.com>
2022-11-22 11:42:07 +0100

    created a controller input

--- lesson4/class-state/src/App.js
+++ lesson4/class-state/src/App.js
@@ -4,18 +4,66 @@ import Counter from "./Counter";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [username, setUsername] = useState("Person");
+
+  function typeHandler(event) {
+    console.log(event.target.value);
+    setUsername(event.target.value);
+  }
+
+  // const people = [
+  //   { name: "Bob" },
+  //   { name: "Lala" },
+  //   { name: "Peter" },
+  //   { name: "Andi" },
+  // ];
+
+  const [inputValue, setInputValue] = useState(" ");
+  const [listPeople, setListPeople] = useState(["Blob", "Chet"]);
+
+  function inputTypeHandler(event) {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  }
+
+  function addingPeopleHandler(event) {
+    console.log(event.target.value);
+    if (event.key === "Enter") {
+      setListPeople([...listPeople, inputValue]);
+      setInputValue("");
+    }
+  }
+
   return (
     <div className="App">
-      <Counter count={count} setCount={setCount} />
-      <br />
-      <br />
-      <Counter count={count} setCount={setCount} />
-      <br />
+      <h1>Hello {username} </h1>
+      <input value={username} onChange={typeHandler} />
       <br />
       <Counter count={count} setCount={setCount} />
       <br />
       <br />
       <Counter count={count} setCount={setCount} />
+      <br />
+      <br />
+      <Counter count={count} setCount={setCount} />
+      <br />
+      <br />
+      <Counter count={count} setCount={setCount} />
+      <br />
+      <ul>
+        {/* {people.map((person) => {
+          return <li>{person.name}</li>;
+        })} */}
+        {listPeople.map((listPerson) => (
+          <li>{listPerson}</li>
+        ))}
+      </ul>
+      <input
+        value={inputValue}
+        onChange={inputTypeHandler}
+        onKeyPress={addingPeopleHandler}
+        type="text"
+      />
     </div>
   );
 }
