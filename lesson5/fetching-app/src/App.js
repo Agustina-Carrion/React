40ee00118a8a8c30026cb9677d9e2f5a2d39ce6b
Agustina <agus.carrion@icloud.com>
2022-11-25 14:13:47 +0100

    updated fetching-app

--- lesson5/fetching-app/src/App.js
+++ lesson5/fetching-app/src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 import CardContent from "./CardContent";
 
 function App() {
-  const [myPokemon, setMyPokemon] = useState([]);
+  const [pokeList, setPokeList] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/8")
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data.name);
-        setMyPokemon(data);
+        console.log(data);
+        setPokeList(data);
       });
   }, []);
 
   return (
     <div className="App">
-      <div>{myPokemon.name}, I choose you!</div>
+      <ul>
+        {pokeList.name}
+      </ul>
       <div class="container">
-        <CardContent myPokemon={myPokemon.name} />
+        <CardContent pokeList={pokeList.name}/>
         <div class="flap"></div>
       </div>
     </div>
--- lesson5/fetching-app/src/CardContent.js
+++ lesson5/fetching-app/src/CardContent.js
@@ -1,14 +1,8 @@
-function CardContent({ myPokemon }) {
+function CardContent({ pokeList }) {
   return (
     <div class="content">
-      <h1>Pratham</h1>
-      <h3>
-        I love designing websites and keep things as simple as possible. My
-        goals is to focus on minimalism and conveying the message that you want
-        to send
-      </h3>
-      <h2>{myPokemon}</h2>
-      <div>{myPokemon}</div>
+      <h1>{pokeList}</h1>
+      <h3>This is a struggle!</h3>
     </div>
   );
 }
